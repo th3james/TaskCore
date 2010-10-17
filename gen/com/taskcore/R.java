b474f592b3b31a57d7e50824af8e2fdcfdeb9cda
James Cox <th3james@gmail.com>
2010-10-17 18:26:45 +0100

    Added context menu

--- gen/com/taskcore/R.java
+++ gen/com/taskcore/R.java
@@ -23,6 +23,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int menu_delete=0x7f040004;
         public static final int menu_insert=0x7f040003;
         public static final int no_tasks=0x7f040002;
     }
--- src/com/taskcore/TaskCore.java
+++ src/com/taskcore/TaskCore.java
@@ -8,15 +8,23 @@ package com.taskcore;
 import android.app.ListActivity;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.SimpleCursorAdapter;
 
 public class TaskCore extends ListActivity {
-	public static final int INSERT_ID = Menu.FIRST;
+    private static final int ACTIVITY_CREATE=0;
+    private static final int ACTIVITY_EDIT=1;
+    
+	private static final int INSERT_ID = Menu.FIRST;
+    private static final int DELETE_ID = Menu.FIRST + 1;
 	
-    private int mTaskNumber = 1;
     private TasksDbAdapter mDbHelper;
+    private Cursor mTasksCursor;
     
     /** Called when the activity is first created. */
     @Override
@@ -26,6 +34,8 @@ public class TaskCore extends ListActivity {
         mDbHelper = new TasksDbAdapter(this);
         mDbHelper.open();
         fillData();
+        
+        registerForContextMenu(getListView());
     }
 
 
@@ -46,24 +56,47 @@ public class TaskCore extends ListActivity {
        
         return super.onOptionsItemSelected(item);
     }
+
+    //Handle creation of context menu
+    @Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+	    menu.add(0, DELETE_ID, 0, R.string.menu_delete);
+	}
+    
+    @Override
+	public boolean onContextItemSelected(MenuItem item) {
+    	switch(item.getItemId()) {
+        case DELETE_ID:
+            AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+            mDbHelper.deleteTask(info.id);
+            fillData();
+            return true;
+        }
+		return super.onContextItemSelected(item);
+	}
     
     private void createTask() {
-        String noteName = "Task " + mTaskNumber++;
+        String noteName = "Task ";
         mDbHelper.createTask(noteName);
         fillData();
     }
     
     private void fillData() {
         // Get all of the notes from the database and create the item list
-        Cursor c = mDbHelper.fetchAllTasks();
-        startManagingCursor(c);
+        mTasksCursor = mDbHelper.fetchAllTasks();
+        startManagingCursor(mTasksCursor);
 
+        // Create an array to specify the fields we want to display in the list (only TITLE)
         String[] from = new String[] { TasksDbAdapter.KEY_NAME };
+        
+        // and an array of the fields we want to bind those fields to (in this case just text1)
         int[] to = new int[] { R.id.text1 };
         
         // Now create an array adapter and set it to display using our row
         SimpleCursorAdapter tasks =
-            new SimpleCursorAdapter(this, R.layout.tasks_row, c, from, to);
+            new SimpleCursorAdapter(this, R.layout.tasks_row, mTasksCursor, from, to);
         setListAdapter(tasks);
     }
 }
